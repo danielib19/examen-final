45fcf98f88f556a8fb79cc9b27a53c09baa800c4
danielib19 <daniel_10hidan@hotmail.com>
2018-12-04 08:55:07 -0500

    he modificado el mensaje en salida

--- sistemasventas/src/sistemasventas.cpp
+++ sistemasventas/src/sistemasventas.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!hola mundo!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
